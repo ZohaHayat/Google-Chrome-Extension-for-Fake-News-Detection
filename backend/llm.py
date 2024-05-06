712f5408e330874b166592240e88ad8caf4b0696
Sabaina <sabaina.kashif2001@gmail.com>
2024-05-06 22:42:53 +0500

    Fixed Parsing Error

--- backend/llm.py
+++ backend/llm.py
@@ -5,9 +5,11 @@ from langchain.agents import Tool
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain.agents import initialize_agent
 from gfc import fact_check_claim
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain import hub
 import re
 
-os.environ["FIREWORKS_API_KEY"] = "ylT5fkGfTQpTSnoq57gQOYtmgYcUsLHrxunYpPBUk5aTAqGE"
+os.environ["FIREWORKS_API_KEY"] = "6O0DLhmko5snHGNppXDZB5FuK5ygtrviQXQIUHzGqMvWxKbj"
 os.environ["SERPER_API_KEY"] = "71dec5a8911e77cf4b02622b06f25e2d3661f1f9"
 
 def get_response(text):
@@ -32,12 +34,18 @@ def get_response(text):
     )
     ]
 
-    agent = initialize_agent(
-    tools, llm, agent="zero-shot-react-description", verbose=True
-    )
-
+    # agent = initialize_agent(
+    # tools, llm, agent="zero-shot-react-description", verbose=True
+    # )
+    
     fact = fact_check_claim(text)
     prompt = f"Here is a news statement: {text}. Classify the news statement as one of the following: True News (the assigned rating should be 1.), Uncertain News (the assigned rating should be 2.), or False News (the assigned rating should be 3.). Provide factual explanations to prove your claim. If the news is false, further classify it as misinformation, propaganda or misleading. Only assign the relevant rating to a particular classification. Here is some supplementary information about the statement: {fact}. The format for assigning rating should be the following: Rating: <rating_value>. The answer sequence should be: Classification, Explanation."
+    agent = create_react_agent(llm, tools, hub.pull("hwchase17/react"))
+    agent_ex = AgentExecutor(
+    agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
+)
+
+    
 
     # removing tokens
     def remove_after(text, delimiter):
@@ -57,22 +65,22 @@ def get_response(text):
         else:
             return None
         
-    x = agent.invoke(prompt)
-    print(x)
+    x = agent_ex.invoke({"input":prompt})
+    print(x,"Model's new output")
     
     cleaned_data = remove_after(x['output'], '<|eot_id|>')
     cleaned_data = clean_text(cleaned_data)
     
     prompt2 = f"Only extract and return the rating number given to the text: {x}. Just give me the number as an answer and don't write an entire sentence."
     
-    rating = clean_text(remove_after((agent.invoke(prompt2))['output'], '<|eot_id|>'))
-    # while (rating==None):
-    #     print("ERROR ERROR ERROR")
-    #     rating = clean_text(remove_after((agent.invoke(prompt2))['output'], '<|eot_id|>'))
-    
-    # print("OUT OF ERROR")
+    rating = clean_text(remove_after((agent_ex.invoke({"input":prompt2}))['output'], '<|eot_id|>'))
     rating = extract_rating(rating)
-    
+    while (rating==None):
+        x = agent_ex.invoke({"input":prompt})
+        cleaned_data = clean_text(remove_after(x['output'], '<|eot_id|>'))
+        prompt2 = f"Only extract and return the rating number given to the text: {x}. Just give me the number as an answer and don't write an entire sentence."
+        rating = extract_rating(clean_text(remove_after((agent_ex.invoke({"input":prompt2}))['output'], '<|eot_id|>')))
+            
     print(rating)
     
     cleaned_data = re.sub(r"Rating: \d+\.", "", cleaned_data)
